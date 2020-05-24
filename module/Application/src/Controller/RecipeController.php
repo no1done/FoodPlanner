4fe32f37089b307b15764d2eb5119b91db2c4456
Ryan Done <clients@ryandone.co.uk>
2020-05-24 17:26:12 +0100

    Add recipe print view

--- module/Application/src/Controller/RecipeController.php
+++ module/Application/src/Controller/RecipeController.php
@@ -7,6 +7,7 @@ namespace Application\Controller;
 use Laminas\Http\Response;
 use Laminas\Mvc\Controller\AbstractActionController;
 use Laminas\Mvc\Plugin\FlashMessenger\FlashMessenger;
+use Laminas\View\Model\ViewModel;
 use Lib\Recipe;
 use Lib\RecipeQuery;
 use Propel\Runtime\ActiveQuery\Criteria;
@@ -111,6 +112,19 @@ class RecipeController extends AbstractActionController
             $this->flashMessenger()->addErrorMessage($e->getMessage());
             return $this->redirect()->toRoute('recipe');
         }
+    }
+
+    public function printAction()
+    {
+        $id = $this->params()->fromRoute('id');
+
+        $recipe = RecipeQuery::create()->findPk($id);
+
+        $view = new ViewModel([
+            'recipe' => $recipe
+        ]);
+        $view->setTerminal(true);
+        return $view;
     }
 
     /**
